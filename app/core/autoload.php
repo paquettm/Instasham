aeaaa72c9ae5a5e512950662093958578947250a
Michel Paquette <mic.paquette@gmail.com>
2022-10-13 20:26:56 -0400

    Fixed for compatibility with linux and window file systems.

--- app/core/autoload.php
+++ app/core/autoload.php
@@ -2,6 +2,7 @@
 //this is 75% of a PSR-4 autoloader
 spl_autoload_register(
 	function ($class_name){
+		$class_name = str_replace('\\',DIRECTORY_SEPARATOR,$class_name);
 		require_once($class_name . ".php");
 	}
 );
